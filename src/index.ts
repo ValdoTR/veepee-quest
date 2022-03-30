c4c31b24a7e612a251f8c798932c1a4055a5b151
ValdoTR <vteixeiraromao@gmail.com>
2022-03-30 18:31:59 +0000

    Deploying to gh-pages from master @ 473e0b2dc78609ac4a313f6357a263c7ec311231 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any;
 const declineGameText = "It's not for me"
@@ -13,17 +13,17 @@ const config: any = {
             {
                 label: "PADLOCK PAGE",
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/forms.gle\/HE7P5Ue3KUyPpf956').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/forms.gle\/HE7P5Ue3KUyPpf956").then(() => closePopup()),
             },
             {
                 label: "GAME INSTRUCTION",
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/VEEPEE_QUEST_GAME_INSTRUCTION.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/VEEPEE_QUEST_GAME_INSTRUCTION.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeAmsterdam: {
-        message: 'Congrats, you just found the orange bicycle, click to discover the riddle of the lock 2',
+        message: "Congrats, you just found the orange bicycle, click to discover the riddle of the lock 2",
         buttons: [
             {
                 label: declineGameText,
@@ -33,12 +33,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-2-CYPHER-GAME-IN-NETHERLANDS-AMSTERDAM-(ORANGE)-BIKE.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-2-CYPHER-GAME-IN-NETHERLANDS-AMSTERDAM-(ORANGE)-BIKE.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeBarcelone: {
-        message: 'Congrats, you just found the Umbrella, click to discover the riddle of the lock 4',
+        message: "Congrats, you just found the Umbrella, click to discover the riddle of the lock 4",
         buttons: [
             {
                 label: declineGameText,
@@ -48,12 +48,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-4-DECODER-DISC-SPAIN-BARCELONA-PARASOL-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-4-DECODER-DISC-SPAIN-BARCELONA-PARASOL-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeBrussels: {
-        message: 'Congrats, you just found the Manneken pis, click to discover the riddle of the lock 5',
+        message: "Congrats, you just found the Manneken pis, click to discover the riddle of the lock 5",
         buttons: [
             {
                 label: declineGameText,
@@ -63,12 +63,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-5-SQUARE-COUNT-GAME-IN-BELGIUM-BRUSSELS-MENNKENPIS-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-5-SQUARE-COUNT-GAME-IN-BELGIUM-BRUSSELS-MENNKENPIS-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     teleportCyber: {
-        message: 'Attention, zone de téléportation !',
+        message: "Attention, zone de téléportation !",
         buttons: [
             {
                 label: "GO!",
@@ -78,7 +78,7 @@ const config: any = {
         ]
     },
     crackTheCodeDusseldorf: {
-        message: 'Congrats, you just found the cartwheels, click to discover the riddle of the lock 3', 
+        message: "Congrats, you just found the cartwheels, click to discover the riddle of the lock 3",
         buttons: [
             {
                 label: declineGameText,
@@ -88,12 +88,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-3-CROSSWORD-GAME-IN-GERMANY-CARTWHEELS-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-3-CROSSWORD-GAME-IN-GERMANY-CARTWHEELS-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeLausanne: {
-        message: 'Congrats, you just found the chocolate, click to discover the riddle of the lock 9',
+        message: "Congrats, you just found the chocolate, click to discover the riddle of the lock 9",
         buttons: [
             {
                 label: declineGameText,
@@ -103,12 +103,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-9-FILL-IN-THE-BLANK-GAME-IN-ZWITZERLAND-LAUSANNE-CHOCOLAT-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-9-FILL-IN-THE-BLANK-GAME-IN-ZWITZERLAND-LAUSANNE-CHOCOLAT-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeMilan: {
-        message: 'Congrats, you just found the football balloon, click to discover the riddle of the lock 8',
+        message: "Congrats, you just found the football balloon, click to discover the riddle of the lock 8",
         buttons: [
             {
                 label: declineGameText,
@@ -118,12 +118,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-8-GUESS-THE-PLACE-IN-ITALY-MILAN-FOOTBALL-BALLOON-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-8-GUESS-THE-PLACE-IN-ITALY-MILAN-FOOTBALL-BALLOON-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeTelaviv: {
-        message: 'Congrats, you just found the skateboard, click to discover the riddle of the lock 6',
+        message: "Congrats, you just found the skateboard, click to discover the riddle of the lock 6",
         buttons: [
             {
                 label: declineGameText,
@@ -133,12 +133,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-6-CRACK-THE-4-DIGITS-IN-ISRAEL-CRACK-THE-4-DIGIT-IN-ISRAEL-TEL-AVIV-SKETEBOARD-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-6-CRACK-THE-4-DIGITS-IN-ISRAEL-CRACK-THE-4-DIGIT-IN-ISRAEL-TEL-AVIV-SKETEBOARD-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeVerone: {
-        message: 'Congrats, you just found the macaron, click to discover the riddle of the lock 1',
+        message: "Congrats, you just found the macaron, click to discover the riddle of the lock 1",
         buttons: [
             {
                 label: declineGameText,
@@ -148,12 +148,12 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-1-A-MAZE-(TH)ING-GAME-IN-FRANCE-MACARON-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-1-A-MAZE-(TH)ING-GAME-IN-FRANCE-MACARON-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
     crackTheCodeWarsaw: {
-        message: 'Congrats, you just found the pierogi, click to discover the riddle of the lock 7',
+        message: "Congrats, you just found the pierogi, click to discover the riddle of the lock 7",
         buttons: [
             {
                 label: declineGameText,
@@ -163,7 +163,7 @@ const config: any = {
             {
                 label: acceptGameText,
                 className: "primary",
-                callback: () => WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-7-REBUS-GAME-IN-POLAND-WARSAW-FOLKLORIC-DOLL-ITEM.pdf').then(() => closePopup()),
+                callback: () => WA.nav.openCoWebSite("https:\/\/valdotr.github.io\/veepee-quest-map\/src\/assets\/game\/RIDDLE-7-REBUS-GAME-IN-POLAND-WARSAW-FOLKLORIC-DOLL-ITEM.pdf").then(() => closePopup()),
             },
         ]
     },
@@ -177,15 +177,14 @@ const IFRAME_Y = 38;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log("Scripting API ready");
+    console.log("Player tags: ",WA.player.tags)
 
     // Open or close popups
     for(let name of Object.keys(config)) {
-        // We assume that each popup and zone have a name in common + 'Popup' for popups and + 'Zone' for layers.
-        const layerName = name + 'Zone'
-        const popupName = name + 'Popup'
-        WA.room.onEnterLayer(layerName).subscribe(() => openPopup(popupName))
+        // We assume that each popup and zone have a name in common + "Popup" for popups and + "Zone" for layers.
+        const layerName = name + "Zone"
+        WA.room.onEnterLayer(layerName).subscribe(() => openPopup(name))
         WA.room.onLeaveLayer(layerName).subscribe(() => {
             closePopup()
             WA.nav.closeCoWebSite();
@@ -194,10 +193,7 @@ WA.onInit().then(() => {
 
     // Olympiad
     const createNewEmbedWebsite = (cinemaScreenVariable: string) => {
-        WA.room.website.get('cinemaScreen').then((holdWebsite) => {
-            console.log('onVariableChange => WA.room.website.get => data', holdWebsite);
-
-            console.log('onVariableChange => WA.room.website.get => then => create');
+        WA.room.website.get("cinemaScreen").then((holdWebsite) => {
             WA.room.website.create({
                 name: holdWebsite && holdWebsite.name ? holdWebsite.name : "cinemaScreen",
                 url: cinemaScreenVariable,
@@ -212,10 +208,8 @@ WA.onInit().then(() => {
                 allow: "fullscreen",
             } as any);
 
-            console.log('onVariableChange => WA.room.website.get => then => delete');
-            WA.room.website.delete('cinemaScreen');
+            WA.room.website.delete("cinemaScreen");
         }).catch((err) => {
-            console.error('onVariableChange => WA.room.website.get => catch => err', err);
             WA.room.website.create({
                 name: "cinemaScreen",
                 url: cinemaScreenVariable,
@@ -233,13 +227,12 @@ WA.onInit().then(() => {
     }
 
 
-    const cinemaScreenVariable = WA.state.loadVariable('cinemaScreenVariable');
+    const cinemaScreenVariable = WA.state.loadVariable("cinemaScreenVariable");
     if(cinemaScreenVariable !== undefined){
         createNewEmbedWebsite(cinemaScreenVariable as string);
     }
 
-    WA.state.onVariableChange('cinemaScreenVariable').subscribe((cinemaScreenVariable: unknown) => {
-        console.log('cinemaScreenVariable => data', cinemaScreenVariable);
+    WA.state.onVariableChange("cinemaScreenVariable").subscribe((cinemaScreenVariable: unknown) => {
         if(cinemaScreenVariable === undefined){
             return;
         }
@@ -248,17 +241,18 @@ WA.onInit().then(() => {
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features
     bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
+        console.log("Scripting API Extra ready");
     }).catch(e => console.error(e));
     
 }).catch(e => console.error(e));
 
 // Popup management functions
-function openPopup(popupName: string) {
-    currentPopup = WA.ui.openPopup(popupName, config[popupName].message, config[popupName].buttons)
+function openPopup(name: string) {
+    const popupName = name + "Popup"
+    currentPopup = WA.ui.openPopup(popupName, config[name].message, config[name].buttons)
 }
 function closePopup() {
-    if (typeof currentPopup !== 'undefined') {
+    if (typeof currentPopup !== "undefined") {
         currentPopup.close();
         currentPopup = undefined;
     }
